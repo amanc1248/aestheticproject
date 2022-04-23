cfbc99743f2db6853626d94bb87e4e46e911560b
amanc1248 <amanc1248@gmail.com>
2022-04-23 16:45:48 +0545

    commiting using heroku

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "aestheticproject",
-  multipleStatements: true,
-});
-// Connect
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("MySql Connected...");
-});
-module.exports = { db };
+// const express = require("express");
+// const mysql = require("mysql");
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "aestheticproject",
+//   multipleStatements: true,
+// });
+// // Connect
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("MySql Connected...");
+// });
+// module.exports = { db };
