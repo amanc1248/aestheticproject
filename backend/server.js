595912c9b0bbd9d4e3d494d124eab0024553c0d3
amanc1248 <amanc1248@gmail.com>
2022-04-23 17:46:35 +0545

    commiting using heroku

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 // app.get("/test", testDb);
+
+app.get("/api", (req, res) => {
+  res.send("Api is running");
+});
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) =>
@@ -15,7 +19,5 @@ if (process.env.NODE_ENV === "production") {
     res.send("Api is running");
   });
 }
-app.get("/api", (req, res) => {
-  res.send("Api is running");
-});
+
 app.listen(PORT, console.log("server is running"));
