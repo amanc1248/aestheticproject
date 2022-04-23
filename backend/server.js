eaf1d24b641c97fbc6df919beec577700d5eb630
amanc1248 <amanc1248@gmail.com>
2022-04-23 17:26:31 +0545

    commiting using heroku

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 // app.get("/test", testDb);
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
