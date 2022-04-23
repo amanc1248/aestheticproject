67ae50beee897f9cd3b4b15423f72ac6b6a3011d
amanc1248 <amanc1248@gmail.com>
2022-04-23 13:56:53 +0545

    ADD: Backend, simple db, and route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const db = require("../database/db.js");
+const { db } = require("../database/db.js");
+const { testDb } = require("./controllers/employee.js");
 const app = express();
+app.get("/test", testDb);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
 app.listen(3001, console.log("server is running"));
