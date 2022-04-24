e03cf917dfe8c3761db6231bf84871e6a311cad7
amanc1248 <amanc1248@gmail.com>
2022-04-24 12:31:44 +0545

    ADD: DB to heroku

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -1,10 +1,10 @@
-// const { db } = require("../../database/db.js");
-// const asyncHandler = require("express-async-handler");
-// // Test employee
-// const testDb = asyncHandler(async (req, res) => {
-//   let sql = "select * from employee;";
-//   db.query(sql, (err, result) => {
-//     res.send(result);
-//   });
-// });
-// module.exports = { testDb };
+const { db } = require("../../database/db.js");
+const asyncHandler = require("express-async-handler");
+// Test employee
+const testDb = asyncHandler(async (req, res) => {
+  let sql = "select * from employee;";
+  db.query(sql, (err, result) => {
+    res.send(result);
+  });
+});
+module.exports = { testDb };
--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
-// const express = require("express");
-// const mysql = require("mysql");
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "aestheticproject",
-//   multipleStatements: true,
-// });
-// // Connect
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("MySql Connected...");
-// });
-// module.exports = { db };
+const express = require("express");
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b0d62e1c262749",
+  password: "fe61b116",
+  database: "heroku_e9cbc65fb336414",
+  multipleStatements: true,
+});
+// Connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySql Connected...");
+});
+module.exports = { db };
