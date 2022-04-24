778597cc979504e8b8b3416b7ccc31e4e2d3ed61
amanc1248 <amanc1248@gmail.com>
2022-04-24 12:33:22 +0545

    ADD: DB to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-// const { db } = require("../database/db.js");
-// const { testDb } = require("./controllers/employee.js");
+const { db } = require("../database/db.js");
+const { testDb } = require("./controllers/employee.js");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// app.get("/test", testDb);
+app.get("/test", testDb);
 
 app.get("/api", (req, res) => {
   res.send("Api is running");
