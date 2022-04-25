8318359e98a4b5ed2bf3156f68da13d037086587
amanc1248 <amanc1248@gmail.com>
2022-04-25 11:04:15 +0545

    add

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
 const mysql = require("mysql");
+
+dotenv.config();
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
