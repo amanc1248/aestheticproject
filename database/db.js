b96b0323193f3798a00b6ee8950fb9ffaa317e07
amanc1248 <amanc1248@gmail.com>
2022-04-25 11:14:05 +0545

    ADD: config vars to heroku

--- database/db.js
+++ database/db.js
@@ -3,18 +3,11 @@ const dotenv = require("dotenv");
 const mysql = require("mysql");
 
 dotenv.config();
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   multipleStatements: true,
-// });
 const db = mysql.createPool({
-  host: "us-cdbr-east-05.cleardb.net",
-  user: "b0d62e1c262749",
-  password: "fe61b116",
-  database: "heroku_e9cbc65fb336414",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 });
 
