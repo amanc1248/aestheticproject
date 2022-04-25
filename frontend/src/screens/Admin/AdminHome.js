77bba6a2cc3573215993bfe264c2d125ed41e686
amanc1248 <amanc1248@gmail.com>
2022-04-25 13:13:09 +0545

    ADD: employee list

--- frontend/src/screens/Admin/AdminHome.js
+++ frontend/src/screens/Admin/AdminHome.js
@@ -15,7 +15,7 @@ function AdminHome() {
           <button className="add__employee__button">Add Employee</button>
         </div>
         <div className="users__container col-lg-6 col-md-12 col-sm-12">
-          Users
+          <AdminEmployees></AdminEmployees>
         </div>
       </div>
     </div>
@@ -23,3 +23,32 @@ function AdminHome() {
 }
 
 export default AdminHome;
+
+function AdminEmployees() {
+  return (
+    <div className="admin__employee__container">
+      <div className="employee__container">
+        <div className="employee_details">
+          <div className="employee__details__inner">
+            <img
+              src="https://www.aesthetic.com/img/homepage/customize.svg"
+              alt="employee"
+              height="40px"
+              className="employee__image"
+            />
+          </div>
+          <div className="employee__name__post">
+            <div className="employee__name">Aman</div>
+            <div className="employee__post">Software developer</div>
+          </div>
+        </div>
+        <div className="">
+          <button className="edit_employee__button">Edit Details</button>
+        </div>
+        <div>
+          <button className="delete__employee__button">Free Employee</button>
+        </div>
+      </div>
+    </div>
+  );
+}
