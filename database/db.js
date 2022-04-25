1e4ced55f84d9b41be0ad40a641df59deff89d6e
amanc1248 <amanc1248@gmail.com>
2022-04-25 11:08:49 +0545

    add

--- database/db.js
+++ database/db.js
@@ -3,11 +3,18 @@ const dotenv = require("dotenv");
 const mysql = require("mysql");
 
 dotenv.config();
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   multipleStatements: true,
+// });
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b0d62e1c262749",
+  password: "fe61b116",
+  database: "heroku_e9cbc65fb336414",
   multipleStatements: true,
 });
 
