e72d904539b217a8a25dfc6cefbc630ef0177810
amanc1248 <amanc1248@gmail.com>
2022-04-26 20:56:16 +0545

    FIX: change password look

--- frontend/src/screens/Admin/ChangeEmployeePassword.js
+++ frontend/src/screens/Admin/ChangeEmployeePassword.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CloseIcon from "@mui/icons-material/Close";
-import "../../styles/screens/Admin.css";
+// import "../../styles/screens/Admin.css";
 function ChangeEmployeePassword({ setChangePassword }) {
   const closeChangePassword = () => {
     setChangePassword(false);
@@ -10,45 +10,51 @@ function ChangeEmployeePassword({ setChangePassword }) {
   const [newPassword, setNewPassword] = useState();
   return (
     <div>
-      <div className="home__admin__login">
-        <div className="employee__add__container">
-          <div className="title__and__close">
-            <div className="admin__login__title">Change Password</div>
-            <div className="close__icon">
-              <CloseIcon onClick={closeChangePassword}></CloseIcon>
+      <div>
+        <div className="home__admin__login">
+          <div className="employee__add__container">
+            <div className="title__and__close">
+              <div className="admin__login__title">Change Password</div>
+              <div className="close__icon">
+                <CloseIcon onClick={closeChangePassword}></CloseIcon>
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="oldpassword" className="admin__login__label">
+                old password<span style={{ color: "red" }}>*</span>
+              </label>
+              <br />
+              <input
+                type="password"
+                id="oldpassword"
+                required
+                onChange={(e) => {
+                  setOldPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="newpassword" className="admin__login__label">
+                new password<span style={{ color: "red" }}>*</span>
+              </label>
+              <br />
+              <input
+                type="password"
+                id="newpassword"
+                required
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
+            </div>
+
+            <div>
+              <button className="login__employee__button">
+                Change Password
+              </button>
             </div>
           </div>
-
-          <label htmlFor="oldpassword" className="admin__login__label">
-            old password<span style={{ color: "red" }}>*</span>
-          </label>
-          <br />
-          <input
-            type="password"
-            id="oldpassword"
-            required
-            onChange={(e) => {
-              setOldPassword(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="newpassword" className="admin__login__label">
-            new password<span style={{ color: "red" }}>*</span>
-          </label>
-          <br />
-          <input
-            type="password"
-            id="newpassword"
-            required
-            onChange={(e) => {
-              setNewPassword(e.target.value);
-            }}
-          />
-        </div>
-
-        <div>
-          <button className="login__employee__button">Change Password</button>
         </div>
       </div>
     </div>
