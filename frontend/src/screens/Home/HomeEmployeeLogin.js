f762b592531fd5c033f4642fcf1eeb68ee149cd8
amanc1248 <amanc1248@gmail.com>
2022-04-27 15:32:59 +0545

    ADD: prepare for employee login

--- frontend/src/screens/Home/HomeEmployeeLogin.js
+++ frontend/src/screens/Home/HomeEmployeeLogin.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import "../../styles/screens/Home.css";
+import { useDispatch } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 function HomeEmployeeLogin({ setAdminLogin }) {
   const closeLogin = () => {
     setAdminLogin(false);
+  };
+
+  // states
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  // handlers
+  const employeeLoginHandler = () => {
+    if (username && password) {
+    }
   };
   return (
     <div className="home__admin__login">
@@ -20,14 +31,28 @@ function HomeEmployeeLogin({ setAdminLogin }) {
               Username<span style={{ color: "red" }}>*</span>
             </label>
             <br />
-            <input type="text" required id="username" />
+            <input
+              type="text"
+              required
+              id="username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
             <br />
             <label htmlFor="password">
               Password
               <span style={{ color: "red" }}>*</span>
             </label>
             <br />
-            <input type="password" id="password" required />
+            <input
+              type="password"
+              id="password"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
           <div>
             <button className="login__employee__button" type="submit">
