f94e355f37578830d1a25066b2129c511206a85b
amanc1248 <amanc1248@gmail.com>
2022-04-27 15:38:11 +0545

    ADD: employeeLoginAction

---
+++ frontend/src/actions/employeeActions.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {
+  EMPLOYEE_LOGIN,
+  EMPLOYEE_LOGIN_FAIL,
+  EMPLOYEE_LOGIN_SUCCESS,
+} from "../constants/employeeConstants";
+
+// admin DELETE EMPLOYEE
+export const employeeLoginAction = (username, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: EMPLOYEE_LOGIN,
+    });
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.get(
+      `/api/employee/employeeLogin`,
+      { username, password },
+      config
+    );
+    dispatch({
+      type: EMPLOYEE_LOGIN_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: EMPLOYEE_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
