dc5bc66eafc667bc2fd98e6f916cef6d6f5b54cb
amanc1248 <amanc1248@gmail.com>
2022-04-27 12:22:06 +0545

    ADD: error cleaning action

--- frontend/src/screens/Admin/ChangeEmployeePassword.js
+++ frontend/src/screens/Admin/ChangeEmployeePassword.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import { useDispatch, useSelector } from "react-redux";
-import { adminChangeEmployeePasswordAction } from "../../actions/adminActions";
+import {
+  adminChangeEmployeePasswordAction,
+  adminChangeEmployeePasswordActionCleanError,
+} from "../../actions/adminActions";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
 // import "../../styles/screens/Admin.css";
 function ChangeEmployeePassword({ setChangePassword, employee }) {
   const dispatch = useDispatch();
   const closeChangePassword = () => {
+    dispatch(adminChangeEmployeePasswordActionCleanError());
     setChangePassword(false);
   };
   // states
@@ -19,7 +23,7 @@ function ChangeEmployeePassword({ setChangePassword, employee }) {
     e.preventDefault();
     dispatch(
       adminChangeEmployeePasswordAction(
-        employee.id,
+        "employee.id",
         "oldPassword",
         "newPassword"
       )
@@ -31,65 +35,60 @@ function ChangeEmployeePassword({ setChangePassword, employee }) {
     (state) => state.adminChangeEmployeePasswordReducer
   );
   return (
-    <div>
-      {loading === true ? (
-        <>
-          <Loader></Loader>
-        </>
-      ) : (
-        <div>
-          <div className="home__admin__login">
-            <div className="employee__add__container">
-              <div className="title__and__close">
-                <div className="admin__login__title">Change Password</div>
-                <div className="close__icon">
-                  <CloseIcon onClick={closeChangePassword}></CloseIcon>
-                </div>
-              </div>
-
-              <div>
-                <label htmlFor="oldpassword" className="admin__login__label">
-                  old password<span style={{ color: "red" }}>*</span>
-                </label>
-                <br />
-                <input
-                  type="password"
-                  id="oldpassword"
-                  required
-                  onChange={(e) => {
-                    setOldPassword(e.target.value);
-                  }}
-                />
-              </div>
-              <div>
-                <label htmlFor="newpassword" className="admin__login__label">
-                  new password<span style={{ color: "red" }}>*</span>
-                </label>
-                <br />
-                <input
-                  type="password"
-                  id="newpassword"
-                  required
-                  onChange={(e) => {
-                    setNewPassword(e.target.value);
-                  }}
-                />
-              </div>
-
-              <div>
-                <button
-                  className="login__employee__button"
-                  onClick={changePasswordHandler}
-                >
-                  Change Password
-                </button>
+    <>
+      <div>
+        <div className="home__admin__login">
+          <div className="employee__add__container">
+            <div className="title__and__close">
+              <div className="admin__login__title">Change Password</div>
+              <div className="close__icon">
+                <CloseIcon onClick={closeChangePassword}></CloseIcon>
               </div>
             </div>
+
+            <div>
+              <label htmlFor="oldpassword" className="admin__login__label">
+                old password<span style={{ color: "red" }}>*</span>
+              </label>
+              <br />
+              <input
+                type="password"
+                id="oldpassword"
+                required
+                onChange={(e) => {
+                  setOldPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="newpassword" className="admin__login__label">
+                new password<span style={{ color: "red" }}>*</span>
+              </label>
+              <br />
+              <input
+                type="password"
+                id="newpassword"
+                required
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
+            </div>
+
+            <div>
+              <button
+                className="login__employee__button"
+                onClick={changePasswordHandler}
+              >
+                Change Password
+              </button>
+            </div>
+            {loading && <Loader></Loader>}
             {error && <Message variant="danger">{error}</Message>}
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 
