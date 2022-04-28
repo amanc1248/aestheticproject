fb91474e90a271258a6d5aebe7cf06613729c8c0
amanc1248 <amanc1248@gmail.com>
2022-04-28 10:49:50 +0545

    ADD:  employee fetch users action

--- frontend/src/actions/employeeActions.js
+++ frontend/src/actions/employeeActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {
+  EMPLOYEE_FETCH_USERS,
+  EMPLOYEE_FETCH_USERS_SUCCESS,
   EMPLOYEE_LOGIN,
   EMPLOYEE_LOGIN_FAIL,
   EMPLOYEE_LOGIN_SUCCESS,
@@ -23,6 +25,33 @@ export const employeeLoginAction = (username, password) => async (dispatch) => {
     );
     dispatch({
       type: EMPLOYEE_LOGIN_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: EMPLOYEE_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// employee FETCH USERS
+export const employeeFetchUsersAction = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: EMPLOYEE_FETCH_USERS,
+    });
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.get(`/api/employee/fetchUsers`, config);
+    dispatch({
+      type: EMPLOYEE_FETCH_USERS_SUCCESS,
       payload: data,
     });
   } catch (error) {
