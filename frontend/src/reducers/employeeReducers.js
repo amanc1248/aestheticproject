affa857fb85cecdd139cbf0f553ec630f8a8e549
amanc1248 <amanc1248@gmail.com>
2022-04-28 10:48:22 +0545

    ADD: employe fetch users reducer

--- frontend/src/reducers/employeeReducers.js
+++ frontend/src/reducers/employeeReducers.js
@@ -1,4 +1,7 @@
 import {
+  EMPLOYEE_FETCH_USERS,
+  EMPLOYEE_FETCH_USERS_FAIL,
+  EMPLOYEE_FETCH_USERS_SUCCESS,
   EMPLOYEE_LOGIN,
   EMPLOYEE_LOGIN_FAIL,
   EMPLOYEE_LOGIN_SUCCESS,
@@ -11,6 +14,20 @@ export const employeeLoginReducer = (state = {}, action) => {
     case EMPLOYEE_LOGIN_SUCCESS:
       return { loading: false, employeeLogin: action.payload };
     case EMPLOYEE_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const employeeFetchUsersReducer = (state = {}, action) => {
+  switch (action.type) {
+    case EMPLOYEE_FETCH_USERS:
+      return { loading: true };
+    case EMPLOYEE_FETCH_USERS_SUCCESS:
+      return { loading: false, fetchedUsers: action.payload };
+    case EMPLOYEE_FETCH_USERS_FAIL:
       return { loading: false, error: action.payload };
 
     default:
