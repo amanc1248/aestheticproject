f04d818e3d15afbd718e58f0b970967a2b7affbf
amanc1248 <amanc1248@gmail.com>
2022-05-01 21:49:08 +0545

    Add: employeeAuth and logout route

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const {
   employeeLoginController,
   employeeFetchUsersController,
+  employeeLogoutController,
 } = require("../controllers/employeeControllers");
+const { ensureEmployeeAuthentication } = require("../middleware/middleWare");
 
 const router = express.Router();
 router.route("/employeeLogin").post(employeeLoginController);
-router.route("/fetchUsers").get(employeeFetchUsersController);
+router
+  .route("/fetchUsers")
+  .get(ensureEmployeeAuthentication, employeeFetchUsersController);
+router.route("/logout").get(employeeLogoutController);
 
 module.exports = router;
