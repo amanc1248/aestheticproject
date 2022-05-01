15432842094136c99891995348883857d7353832
amanc1248 <amanc1248@gmail.com>
2022-05-01 21:52:14 +0545

    Implemented: employeeAuthentication in frontend

--- frontend/src/screens/Employee/EmployeeUsers.js
+++ frontend/src/screens/Employee/EmployeeUsers.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { employeeFetchUsersAction } from "../../actions/employeeActions";
+import {
+  employeeFetchUsersAction,
+  employeeFetchUsersClean,
+  employeeLoginClean,
+} from "../../actions/employeeActions";
 import PopularNFTs from "./PopularNFTs";
 import RecentlySold from "./RecentlySold";
 import Loader from "../../components/Loader";
+import { useNavigate } from "react-router-dom";
 function EmployeeUsers() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   // states
   const [popularNFTs, setPopularNFTs] = useState(false);
@@ -16,15 +22,22 @@ function EmployeeUsers() {
     setRecentlySold(true);
   };
 
-  // useEffect
-  useEffect(() => {
-    dispatch(employeeFetchUsersAction());
-  }, [dispatch]);
-
   // useSelectors
   const { loading, fetchedUsers } = useSelector(
     (state) => state.employeeFetchUsersReducer
   );
+
+  // useEffects
+  useEffect(() => {
+    dispatch(employeeFetchUsersAction());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (fetchedUsers === "unAuthorized") {
+      // dispatch(employeeFetchUsersClean());
+      navigate("/auth/false/true");
+    }
+  }, [fetchedUsers, navigate, dispatch]);
 
   return (
     <>
@@ -32,51 +45,58 @@ function EmployeeUsers() {
         <Loader></Loader>
       ) : fetchedUsers === "no users" ? (
         <h1>No Users</h1>
+      ) : fetchedUsers === "unAuthorized" ? (
+        <h1>unAuthorized</h1>
+      ) : fetchedUsers === undefined ? (
+        <h1>undefined</h1>
+      ) : fetchedUsers === null ? (
+        <h1>null</h1>
       ) : (
-        fetchedUsers &&
-        fetchedUsers.map((user) => {
-          return (
-            <div>
-              {" "}
-              <div className="admin__employee__container">
-                {popularNFTs && (
-                  <PopularNFTs setPopularNFTs={setPopularNFTs}></PopularNFTs>
-                )}
-                {recentlySold && (
-                  <RecentlySold
-                    setRecentlySold={setRecentlySold}
-                  ></RecentlySold>
-                )}
-                <div className="employee__container">
-                  <div className="employee_details">
-                    <div className="employee__name__post">
-                      <div className="employee__name">{user.name}</div>
+        <>
+          {fetchedUsers.map((user) => {
+            return (
+              <div key={user.id}>
+                {" "}
+                <div className="admin__employee__container">
+                  {popularNFTs && (
+                    <PopularNFTs setPopularNFTs={setPopularNFTs}></PopularNFTs>
+                  )}
+                  {recentlySold && (
+                    <RecentlySold
+                      setRecentlySold={setRecentlySold}
+                    ></RecentlySold>
+                  )}
+                  <div className="employee__container">
+                    <div className="employee_details">
+                      <div className="employee__name__post">
+                        <div className="employee__name">{user.name}</div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="">
-                    <div>
-                      <button
-                        className="edit_employee__button"
-                        onClick={showPopularNFTs}
-                      >
-                        Send Most Popular NFTs of this week.
-                      </button>{" "}
-                    </div>
-                    <br />
-                    <div>
-                      <button
-                        className="delete__employee__button"
-                        onClick={showRecentlySold}
-                      >
-                        Send recently sold NFTs of this week.
-                      </button>
+                    <div className="">
+                      <div>
+                        <button
+                          className="edit_employee__button"
+                          onClick={showPopularNFTs}
+                        >
+                          Send Most Popular NFTs of this week.
+                        </button>{" "}
+                      </div>
+                      <br />
+                      <div>
+                        <button
+                          className="delete__employee__button"
+                          onClick={showRecentlySold}
+                        >
+                          Send recently sold NFTs of this week.
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })
+            );
+          })}
+        </>
       )}
     </>
   );
