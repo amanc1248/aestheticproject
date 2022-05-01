afae9dadaff20dd90a419a7b6b8fe7e1ac63548c
amanc1248 <amanc1248@gmail.com>
2022-05-01 21:50:52 +0545

    Add: employee Logout reducere

--- frontend/src/reducers/employeeReducers.js
+++ frontend/src/reducers/employeeReducers.js
@@ -1,10 +1,16 @@
 import {
   EMPLOYEE_FETCH_USERS,
+  EMPLOYEE_FETCH_USERS_CLEAN,
   EMPLOYEE_FETCH_USERS_FAIL,
   EMPLOYEE_FETCH_USERS_SUCCESS,
   EMPLOYEE_LOGIN,
+  EMPLOYEE_LOGIN_CLEAN,
   EMPLOYEE_LOGIN_FAIL,
   EMPLOYEE_LOGIN_SUCCESS,
+  EMPLOYEE_LOGOUT,
+  EMPLOYEE_LOGOUT_CLEAN,
+  EMPLOYEE_LOGOUT_FAIL,
+  EMPLOYEE_LOGOUT_SUCCESS,
 } from "../constants/employeeConstants";
 
 export const employeeLoginReducer = (state = {}, action) => {
@@ -15,6 +21,8 @@ export const employeeLoginReducer = (state = {}, action) => {
       return { loading: false, employeeLogin: action.payload };
     case EMPLOYEE_LOGIN_FAIL:
       return { loading: false, error: action.payload };
+    case EMPLOYEE_LOGIN_CLEAN:
+      return { employeeLogin: null };
 
     default:
       return state;
@@ -29,6 +37,24 @@ export const employeeFetchUsersReducer = (state = {}, action) => {
       return { loading: false, fetchedUsers: action.payload };
     case EMPLOYEE_FETCH_USERS_FAIL:
       return { loading: false, error: action.payload };
+    case EMPLOYEE_FETCH_USERS_CLEAN:
+      return { fetchedUsers: null };
+
+    default:
+      return state;
+  }
+};
+
+export const employeeLogoutReducer = (state = {}, action) => {
+  switch (action.type) {
+    case EMPLOYEE_LOGOUT:
+      return { loading: true };
+    case EMPLOYEE_LOGOUT_SUCCESS:
+      return { loading: false, employeeLogout: action.payload };
+    case EMPLOYEE_LOGOUT_FAIL:
+      return { loading: false, error: action.payload };
+    case EMPLOYEE_LOGOUT_CLEAN:
+      return { employeeLogout: null };
 
     default:
       return state;
