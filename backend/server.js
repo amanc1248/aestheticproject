8f643381c6899856e63096554d1a36b42d1d3e20
amanc1248 <amanc1248@gmail.com>
2022-05-06 14:35:00 +0545

    IMPLEMENT: express-mysql-session

--- backend/server.js
+++ backend/server.js
@@ -6,37 +6,53 @@ const adminRoutes = require("./routes/adminRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
 const { notFound, errorHandler } = require("./middleware/middleWare.js");
 const { ensureAdminAuthentication } = require("./middleware/middleWare.js");
+const mysql = require("mysql");
 const session = require("express-session");
-const store = new session.MemoryStore();
-dotenv.config();
+const MySQLStore = require("express-mysql-session")(session);
 const app = express();
+dotenv.config();
+const options = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+};
+
+var connection = mysql.createPool(options);
+const sessionStore = new MySQLStore({}, connection);
+
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // session
-const adminSessoin = session({
+const adminSession = session({
   secret: process.env.ADMIN_SECRET,
   resave: false,
   saveUninitialized: false,
-  store,
+  store: sessionStore,
+  name: "adminSession",
   cookie: {
-    maxAge: 600000,
+    maxAge: 40000,
     secure: false,
   },
 });
 
+// const employeeSession
 const employeeSession = session({
-  secret: process.env.EMPLOYEE_SECRET,
+  secret: "employeeSession",
   resave: false,
   saveUninitialized: false,
-  store,
+  store: sessionStore,
+  name: "employeeSession",
+
   cookie: {
-    maxAge: 600000,
+    maxAge: 40000,
     secure: false,
   },
 });
 
-app.use("/api/admin", adminSessoin, adminRoutes);
+app.use("/api/admin", adminSession, adminRoutes);
 app.use("/api/employee", employeeSession, employeeRoutes);
 
 app.get("/api", (req, res) => {
