2b5ce543b02e88037a0f1629a5d2013d08f080a9
amanc1248 <amanc1248@gmail.com>
2022-05-08 22:17:46 +0545

    ADD & FiX css for Loader main

--- frontend/src/components/LoaderMain.js
+++ frontend/src/components/LoaderMain.js
@@ -3,7 +3,9 @@ import "../styles/screens/Home.css";
 const LoaderMain = () => {
   return (
     <div className="main__loader">
-      <div className="main__loader__text">A</div>
+      <div>
+        <div className="main__loader__text">A</div>
+      </div>
     </div>
   );
 };
