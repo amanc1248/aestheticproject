2cb3b332dd2104153e9994dccc4fbfb590f9328d
amanc1248 <amanc1248@gmail.com>
2022-05-16 15:40:33 +0545

    ADD: boxLoader component

---
+++ frontend/src/components/BoxLoader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/components/BoxLoader.css";
+function BoxLoader() {
+  return (
+    <div>
+      <div className="box_loader_title"></div>
+      <div className="box_loader_input"></div>
+      <div className="box_loader_title"></div>
+      <div className="box_loader_input"></div>
+      <div className="box_loader_title"></div>
+      <div className="box_loader_input"></div>
+      <div className="box_loader_title"></div>
+      <div className="box_loader_input"></div>
+    </div>
+  );
+}
+
+export default BoxLoader;
